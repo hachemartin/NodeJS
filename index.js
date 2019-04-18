4c632931c478131e56767e0bc03fa1fcea8bed97
David Hernández <daernandez@gmail.com>
2019-04-18 21:55:19 +0200

    file added

---
+++ index.js
@@ -0,0 +1,14 @@
+// Nodejs trabaja con módulos
+const math = require('./math.js');
+
+console.log(math)
+// el resultado es un objeto con múltiples propiedades. cada propiedad es una función
+
+/*
+console.log(math.add(1,5));
+console.log(math.substract(1,5));
+console.log(math.multiply(1,5));
+console.log(math.divide(1,0));
+*/
+
+//console.log(hello);
